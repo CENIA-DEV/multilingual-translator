b47d4b37497f9e5d057067e04a6d97e8f783aa6d
MartinPC-uls <martin.pizarro@userena.cl>
2024-12-16 12:00:54 -0300

    made changes to support label smoothing

--- src/model.py
+++ src/model.py
@@ -1,10 +1,9 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from accmt import AcceleratorModule
-from transformers import AutoModelForSeq2SeqLM
-
 from arguments import TrainingArguments
-
+from transformers import AutoModelForSeq2SeqLM
 
 class Translator(AcceleratorModule):
     def __init__(self, args: TrainingArguments, config, tokenizer):
@@ -16,38 +15,41 @@ class Translator(AcceleratorModule):
         for param in self.model.parameters():
             if not param.is_contiguous():
                 param.data = param.data.contiguous()
-
+        
         self.tokenizer = tokenizer
         self.vocab_size = config.vocab_size
         self.alpha = args.RDROP_ALPHA
-
-    def compute_kl_loss(self, p, q, pad_mask=None):
+        
+    def compute_kl_loss(self, p, q, pad_mask=None):    
         p_loss = F.kl_div(
-            F.log_softmax(p, dim=-1), F.softmax(q, dim=-1), reduction="none"
+            F.log_softmax(p, dim=-1), F.softmax(q, dim=-1), reduction='none'
         )
         q_loss = F.kl_div(
-            F.log_softmax(q, dim=-1), F.softmax(p, dim=-1), reduction="none"
+            F.log_softmax(q, dim=-1), F.softmax(p, dim=-1), reduction='none'
         )
-
+        
         if pad_mask is not None:
-            p_loss.masked_fill_(pad_mask, 0.0)
-            q_loss.masked_fill_(pad_mask, 0.0)
+            p_loss.masked_fill_(pad_mask, 0.)
+            q_loss.masked_fill_(pad_mask, 0.)
 
         p_loss = p_loss.mean()
         q_loss = q_loss.mean()
 
         loss = (p_loss + q_loss) / 2
         return loss
-
+    
     def compute_loss(self, logits, labels, label_smoothing=0.0):
         return F.cross_entropy(
             logits.view(-1, self.vocab_size),
             labels.view(-1),
-            label_smoothing=label_smoothing,
+            label_smoothing=label_smoothing
         )
-
+    
     def training_step(self, batch):
         batch["labels"][batch["labels"] == self.tokenizer.pad_token_id] = -100
+        compute_loss_kwargs = {
+            "labels": batch["labels"], "label_smoothing": self.args.LABEL_SMOOTHING
+        }
         if self.args.RDROP:
             output1 = self.model(**batch)
             output2 = self.model(**batch)
@@ -56,22 +58,23 @@ class Translator(AcceleratorModule):
                 output1_loss = output1.loss
                 output2_loss = output2.loss
             else:
-                compute_loss_kwargs = {
-                    "labels": batch["labels"],
-                    "label_smoothing": self.args.LABEL_SMOOTHING,
-                }
                 output1_loss = self.compute_loss(output1.logits, **compute_loss_kwargs)
                 output2_loss = self.compute_loss(output2.logits, **compute_loss_kwargs)
 
             nll_loss = 0.5 * (output1_loss + output2_loss)
             kl_loss = self.compute_kl_loss(output1.logits, output2.logits)
-
+            
             loss = nll_loss + self.alpha * kl_loss
 
             return loss
 
-        return self.model(**batch).loss
-
+        output = self.model(**batch)
+        if not self.args.LABEL_SMOOTHING:
+            return output.loss
+        
+        smoothed_loss = self.compute_loss(output.logits, **compute_loss_kwargs)
+        return smoothed_loss
+    
     def validation_step(self, batch):
         directions = batch
         return_dict = dict()
